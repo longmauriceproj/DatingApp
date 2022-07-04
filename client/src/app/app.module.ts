8a108d9dfbec70befccfe85294ea7916641ca4a3
Maurice Long <longmaurice255@gmail.com>
2022-07-04 12:19:05 -0700

    added a register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
