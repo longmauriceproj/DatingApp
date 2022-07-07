75880443c25602a238643f4f37047d9e6f4d457c
Maurice Long <longmaurice255@gmail.com>
2022-07-07 14:18:26 -0700

    implemented functionality to delete photos in client

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -71,4 +71,10 @@ export class PhotoEditorComponent implements OnInit {
       });
     });
   }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter((p) => p.id !== photoId);
+    });
+  }
 }
