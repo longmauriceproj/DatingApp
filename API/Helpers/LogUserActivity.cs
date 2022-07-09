4696d153ab4bd31fe84807c6741251975d73c83b
Maurice Long <longmaurice255@gmail.com>
2022-07-08 21:24:05 -0700

    implemented last active action filter

---
+++ API/Helpers/LogUserActivity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            //using guard clause
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+
+            var userId = resultContext.HttpContext.User.GetUserId();
+            //using service locator pattern
+            var repo = resultContext.HttpContext.RequestServices.GetService<IUserRepository>();
+            var user = await repo.GetUserByIdAsync(userId);
+            user.LastActive = DateTime.Now;
+            await repo.SaveAllAsync();
+        }
+    }
+}
