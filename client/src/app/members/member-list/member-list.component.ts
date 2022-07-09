4e2f23e587fed38a7f2335dee0620465caa10b7c
Maurice Long <longmaurice255@gmail.com>
2022-07-08 17:07:24 -0700

    implemented ngxbootstrap pagination component

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -29,4 +29,9 @@ export class MemberListComponent implements OnInit {
         this.pagination = response.pagination;
       });
   }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadMembers();
+  }
 }
