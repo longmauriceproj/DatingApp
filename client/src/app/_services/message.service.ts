bb9c92a0f6347195d6be4ee7b6ad83e1112174ab
Maurice Long <longmaurice255@gmail.com>
2022-07-13 16:46:40 -0700

    added message thread to client and styled message thread

---
+++ client/src/app/_services/message.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Message } from '../_models/message';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getMessages(pageNumber: number, pageSize: number, container) {
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('Container', container);
+    return getPaginatedResult<Message[]>(
+      `${this.baseUrl}messages`,
+      params,
+      this.http
+    );
+  }
+
+  //could make this paginated
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(
+      `${this.baseUrl}messages/thread/${username}`
+    );
+  }
+}
