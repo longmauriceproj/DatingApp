941636b1dae0bf9ffb608477a0c8d70c9b6915c1
Maurice Long <longmaurice255@gmail.com>
2022-07-21 13:45:59 -0700

    created message hub in API and implemented send message method to the hub

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,6 +58,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
