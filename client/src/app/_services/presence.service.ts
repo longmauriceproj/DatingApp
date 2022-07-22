f4e15dfd70433c9f6cdcef444d359b13f1840443
Maurice Long <longmaurice255@gmail.com>
2022-07-21 20:27:52 -0700

    completed live message and online presence functionality in client and API

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -4,6 +4,8 @@ import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
+import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +16,7 @@ export class PresenceService {
   private onlineUsersSource = new BehaviorSubject<string[]>([]);
   onlineUsers$ = this.onlineUsersSource.asObservable();
 
-  constructor(private toastr: ToastrService) {}
+  constructor(private toastr: ToastrService, private router: Router) {}
 
   createHubConnection(user: User) {
     this.hubConnection = new HubConnectionBuilder()
@@ -27,19 +29,32 @@ export class PresenceService {
     this.hubConnection.start().catch(error => console.error(error));
 
     this.hubConnection.on('UserIsOnline', username => {
-      this.toastr.info(`${username} has connected.`);
+      this.onlineUsers$.pipe(take(1)).subscribe(usernames => {
+        this.onlineUsersSource.next([...usernames, username]);
+      });
     });
 
     this.hubConnection.on('UserIsOffline', username => {
-      this.toastr.warning(`${username} has diconnected.`);
+      this.onlineUsers$.pipe(take(1)).subscribe(usernames => {
+        this.onlineUsersSource.next([...usernames.filter(u => u !== username)]);
+      });
     });
 
     this.hubConnection.on('GetOnlineUsers', (usernames: string[]) => {
       this.onlineUsersSource.next(usernames);
     });
+
+    this.hubConnection.on('NewMessageReceived', ({ username, knownAs }) => {
+      this.toastr
+        .info(`${knownAs} has sent you a new message!`)
+        .onTap.pipe(take(1))
+        .subscribe(() =>
+          this.router.navigateByUrl(`/members/${username}?tab=3`)
+        );
+    });
   }
 
   stopHubConnection() {
-    this.hubConnection.stop().catch(error => console.error(error));
+    this.hubConnection.stop().catch(error => console.log(error));
   }
 }
